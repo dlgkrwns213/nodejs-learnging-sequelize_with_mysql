4f3b463676e56345d4bb2d30c5413500d7c8e6a7
이학준 <dlgkrwns213@naver.com>
2025-02-17 01:20:10 +0900

    Create DB Models

--- models/index.js
+++ models/index.js
@@ -8,4 +8,13 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 db.sequelize = sequelize;
 
+db.User = User;
+db.Comment = Comment;
+
+User.initate(sequelize);
+Comment.initate(sequelize);
+
+User.associate(db);
+Comment.associate(db);
+
 module.exports = db;
---
+++ models/user.js
@@ -0,0 +1,44 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+  static initate(sequelize) {
+    User.init({
+      name: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+      married: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+
+    }, {
+      sequelize,
+      timestamps: false,
+      underscored: false,
+      modelName: "User",
+      tableName: "users",
+      paranoid: false,
+      charset: "utf8",
+      collage: "utf8_general_ci",
+    });
+  }
+
+  static associate(db) {};
+}
+
+models.exports = User;
