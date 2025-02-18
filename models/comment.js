50ecc502b49696f618700b168f4ac93089d1f794
이학준 <dlgkrwns213@naver.com>
2025-02-18 17:32:45 +0900

    Alert

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 class Comment extends Sequelize.Model {
   static initiate(sequelize) {
@@ -11,20 +11,20 @@ class Comment extends Sequelize.Model {
         type: Sequelize.DATE,
         allowNull: true,
         defaultValue: Sequelize.NOW,
-      }
+      },
     }, {
-        sequelize,
-        timestamps: false,
-        modelName: "Comment",
-        tableName: "comments",
-        paranoid: false,
-        charset: "utf8",
-        collate: "utf8mb4_general_ci",
+      sequelize,
+      timestamps: false,
+      modelName: 'Comment',
+      tableName: 'comments',
+      paranoid: false,
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
     });
   }
 
   static associate(db) {
-    db.Comment.belongsTo(db.User, { foreigneKey: "commenter", targetKey: "id" });
+    db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
   }
 };
 
--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 class User extends Sequelize.Model {
   static initiate(sequelize) {
@@ -20,27 +20,26 @@ class User extends Sequelize.Model {
         type: Sequelize.TEXT,
         allowNull: true,
       },
-      create_at: {
+      created_at: {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: Sequelize.NOW,
       },
-
     }, {
       sequelize,
       timestamps: false,
       underscored: false,
-      modelName: "User",
-      tableName: "users",
+      modelName: 'User',
+      tableName: 'users',
       paranoid: false,
-      charset: "utf8",
-      collage: "utf8_general_ci",
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
     });
   }
 
   static associate(db) {
-    db.User.hasMany(db.Comment, {foreignKey: "commenter", sourceKey: "id"});
-  };
-}
+    db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+  }
+};
 
 module.exports = User;
